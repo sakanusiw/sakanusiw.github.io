0790a116ba18ce4227e295175e782a8e50193c6b
sakanusiw <saka.interisti@gmail.com>
2023-05-18 07:28:25 +0700

    Contact Form done

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ function openmenu(){
 function closemenu(){
   sidemenu.style.right = "-200px";
 }
+
+function refreshPage(){
+  window.location.reload();
+}
+
+// Contact Form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyxL0eXLq-2rM7xxgE6diyI5y8Xyb9-l0DRGdn-PeQy2wgS8H17Aeme82vbt_0CDsiN1A/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message sent"
+      setTimeout(function(){
+        msg.innerHTML = ""
+      },5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
